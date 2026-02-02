65fc1654142a451beaea5561f59705799d4d7eaf
stivev73 <stivie73@gmail.com>
2026-02-02 22:35:03 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 class URLS:
-    MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/' #URL главной страницы
-    AUTH_PAGE_URL = 'https://stellarburgers.nomoreparties.site/login' #URL страницы авторизации
-    REG_PAGE_URL = 'https://stellarburgers.nomoreparties.site/register' #URL страницы регистрации
-    RECOVER_PAGE_URL = 'https://stellarburgers.nomoreparties.site/forgot-password' #URL страницы восстановления пароля
-    PROFILE_PAGE_URL = 'https://stellarburgers.nomoreparties.site/account/profile' #URL страницы личного кабинета
+    MAIN_PAGE_URL = 'https://stellarburgers.education-services.ru/' #URL главной страницы
+    AUTH_PAGE_URL = 'https://stellarburgers.education-services.ru/login' #URL страницы авторизации
+    REG_PAGE_URL = 'https://stellarburgers.education-services.ru/register' #URL страницы регистрации
+    RECOVER_PAGE_URL = 'https://stellarburgers.education-services.ru/forgot-password' #URL страницы восстановления пароля
+    PROFILE_PAGE_URL = 'https://stellarburgers.education-services.ru/account/profile' #URL страницы личного кабинета
